f17629de1d6f497af782e946bfe3ad703628908b
jhansi5678 <146322519+jhansi5678@users.noreply.github.com>
2023-09-28 13:03:39 +0530

    Add files via upload
    initial commit to aray.java

---
+++ Array.java
@@ -0,0 +1,17 @@
+package Baggies;
+
+import java.util.Arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int a[]= {5,3,2,4,1,6};
+		Arrays.sort(a);
+			System.out.println("print:");
+			for(int s:a) {
+				System.out.println(s);
+			}
+
+	}
+
+}
